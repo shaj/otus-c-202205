a4e74acd66b9935ebaae747dcf2c44dab11a8787
ShajTester <kolobrod2002@yandex.ru>
2022-07-14 18:30:30 +0300

    HW4: WIP

--- HW4-libcurl/src/main.c
+++ HW4-libcurl/src/main.c
@@ -22,6 +22,34 @@ void print_version(const char *prog_name)
            PROJECT_VERSION_MINOR, PROJECT_VERSION_PATCH);
 }
 
+struct MemoryStruct
+{
+    char *memory;
+    size_t size;
+};
+
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb,
+                                  void *userp)
+{
+    size_t realsize = size * nmemb;
+    struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+    char *ptr = realloc(mem->memory, mem->size + realsize + 1);
+    if (!ptr)
+    {
+        /* out of memory! */
+        printf("not enough memory (realloc returned NULL)\n");
+        return 0;
+    }
+
+    mem->memory = ptr;
+    memcpy(&(mem->memory[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size] = 0;
+
+    return realsize;
+}
+
 int main(int argc, char const *argv[])
 {
     // Проверка аргументов командной строки
@@ -37,6 +65,44 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
 
+    CURL *curl_handle;
+    CURLcode res;
+
+    struct MemoryStruct chunk;
+
+    chunk.memory = malloc(1); /* will be grown as needed by the realloc above */
+    chunk.size = 0;           /* no data at this point */
+
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl_handle = curl_easy_init();
+
+    curl_easy_setopt(curl_handle, CURLOPT_URL, "https://www.example.com/");
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+    res = curl_easy_perform(curl_handle);
+
+    /* check for errors */
+    if (res != CURLE_OK)
+    {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+    }
+    else
+    {
+        /*
+         * Now, our chunk.memory points to a memory block that is chunk.size
+         * bytes big and contains the remote file.
+         *
+         * Do something nice with it!
+         */
+
+        printf("%lu bytes retrieved\n", (unsigned long)chunk.size);
+    }
+
+    curl_easy_cleanup(curl_handle);
+    curl_global_cleanup();
 
     return EXIT_SUCCESS;
 }
